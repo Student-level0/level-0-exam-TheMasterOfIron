815ba6861206cb936c7807ffbdf2583c37da6d4d
TheMasterOfIron <League@AdministorsiMac.attlocal.net>
2018-04-22 11:24:02 -0700

    LEVEL 1!!!!!
    YAYYYYYYYYYY

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -5,15 +5,45 @@ import org.jointheleague.graphical.robot.Robot;
 /* Level 0 Exam: Coding  Exercise #1 */
 public class CodingExercise1 {
 
-    public static void main(String[] args) {
-   	 // 3. ask the user what color they would like the Robot to draw
+	public static void main(String[] args) {
+		Robot robo = new Robot();
+		// 3. ask the user what color they would like the Robot to draw
 
-   	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
+		// 4. use an if/else statement to set the pen color that the user requested
+		// (minimum of 2 colors)
 
-   	 // 2. set the pen width to 10
+		// 2. set the pen width to 10
 
-   	 // 1. make the Robot draw a shape
-   	 
-    }
+		// 1. make the Robot draw a shape
+		robo.hide();
+		robo.setPenWidth(10);
+		robo.penDown();
+		robo.setSpeed(5);
+		for (int i = 0; i < 1000000; i++) {
+			String color = JOptionPane.showInputDialog("What color do you want your square to be?");
+			if (color.equalsIgnoreCase("Red")) {
+				robo.setPenColor(255, 0, 0);
+			} else if (color.equalsIgnoreCase("Blue")) {
+				robo.setPenColor(0, 0, 255);
+			} else if (color.equalsIgnoreCase("Green")) {
+				robo.setPenColor(0, 255, 0);
+			} else if (color.equalsIgnoreCase("White")) {
+				robo.setPenColor(255, 255, 255);
+			} else if (color.equalsIgnoreCase("Black")) {
+				robo.setPenColor(0, 0, 0);
+			} else {
+				JOptionPane.showMessageDialog(null, "Sorry i dont have that color.");
+			}
+			robo.move(100);
+			robo.turn(90);
+			robo.move(100);
+			robo.turn(90);
+			robo.move(100);
+			robo.turn(90);
+			robo.move(100);
+			robo.turn(90);
+		}
+
+	}
 
 }
--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -1,17 +1,26 @@
 
-
 /* Level 0 Exam: Coding  Exercise #2 */
 import javax.swing.JOptionPane;
 
 /**
- * Write a program that asks the user for their age. 
- * Tell them which year they were born. 
- * If they are over 30, tell them they are too old to play this game.
+ * Write a program that asks the user for their age. Tell them which year they
+ * were born. If they are over 30, tell them they are too old to play this game.
  **/
 
 public class CodingExercise2 {
-
+public static void main(String[] args) {
+	int year = 2018;
+	for (int i = 0; i < 1000000; i++) {
+		String SAge = JOptionPane.showInputDialog("How old are you.");
+	int IAge = Integer.parseInt(SAge);
+	if (IAge > 30) {
+		JOptionPane.showMessageDialog(null, "You are too old to play this game.");
+	}
+	else {
+		int IBorn = year - IAge;
+		JOptionPane.showMessageDialog(null, "You were born in " + IBorn + ".");
+	}
+	}
+	
 }
-
-
-
+}
