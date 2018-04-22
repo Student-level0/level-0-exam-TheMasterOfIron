01742284a6338132f816e7497e8d4b228e8f9d30
TheMasterOfIron <League@AdministorsiMac.attlocal.net>
2018-04-22 11:25:15 -0700

    LEVEL 1???
    YAYYY

--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -8,19 +8,18 @@ import javax.swing.JOptionPane;
  **/
 
 public class CodingExercise2 {
-public static void main(String[] args) {
-	int year = 2018;
-	for (int i = 0; i < 1000000; i++) {
-		String SAge = JOptionPane.showInputDialog("How old are you.");
-	int IAge = Integer.parseInt(SAge);
-	if (IAge > 30) {
-		JOptionPane.showMessageDialog(null, "You are too old to play this game.");
+	public static void main(String[] args) {
+		int year = 2018;
+		for (int i = 0; i < 1000000; i++) {
+			String SAge = JOptionPane.showInputDialog("How old are you.");
+			int IAge = Integer.parseInt(SAge);
+			if (IAge > 30) {
+				JOptionPane.showMessageDialog(null, "You are too old to play this game.");
+			} else {
+				int IBorn = year - IAge;
+				JOptionPane.showMessageDialog(null, "You were born in " + IBorn + ".");
+			}
+		}
+
 	}
-	else {
-		int IBorn = year - IAge;
-		JOptionPane.showMessageDialog(null, "You were born in " + IBorn + ".");
-	}
-	}
-	
-}
 }
